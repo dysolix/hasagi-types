669483fa730171e410d36ce43663ea4430db6987
Felix Matenar <contact@dysolix.dev>
2024-04-03 16:39:38 +0200

    Updated to 14.7

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
 export type { LCUEndpoint, LCUEndpointBodyType, LCUEndpointResponseType, LCUEndpoints, HttpMethod, EndpointsWithMethod } from "./lcu-endpoints";
-export type { LCUTypes } from "./lcu-types";
+export type * as LCUTypes from "./lcu-types";
 export type { LCUWebSocketEvents } from "./lcu-events";
